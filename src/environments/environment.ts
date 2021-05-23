5c07dbc8993accac54a5dabc8cd587849166d731
Mary Nyamor <mary.nyamor@canter.fi>
2021-05-23 23:55:11 +0300

    add environments configs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '1cb6ace31e50401f28b864f0b23fdc68',
+  apiUrl: 'https://api.openweathermap.org/data/2.5'
 };
 
 /*
